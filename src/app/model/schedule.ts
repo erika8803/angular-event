04877937f0a2f4c57297626e1772611edc69393d
erika8803 <003erk@gmail.com>
2023-02-23 16:05:44 +0900

    update delete追加

--- src/app/model/schedule.ts
+++ src/app/model/schedule.ts
@@ -1,11 +1,22 @@
 import { User } from './user';
-
 export class Schedule {
-  constructor(
-    public user: User,
-    public eventDate: string,
-    public eventTitle: string,
-    public eventComment: string
-  ) { }
+
+  user: User;
+  eventDate: string;
+  eventTitle: string;
+  eventComment: string;
+  key?: string;
+  isEdit: boolean;
+
+  constructor(value: any) {
+    this.user = value.user;
+    this.eventDate = value.eventDate;
+    this.eventTitle = value.eventTitle;
+    this.eventComment = value.eventComment;
+    if (value.key) {
+      this.key = value.key;
+    }
+
+  }
 
 }
